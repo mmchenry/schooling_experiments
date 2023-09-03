f71311b21ab3f33b8ac4d74c7ec0aab3d6774030
mmchenry <mmchenry@uci.edu>
2023-09-03 15:39:21 -0700

    Fix to copy_files

---
+++ copy_files.py
@@ -0,0 +1,83 @@
+""" Copies over all centroid files and experiment_log.csv files from the TRex on Vortex to a local directory. """
+
+import os
+import shutil
+
+# Source and destination root directories
+src_root = "/Volumes/schooling/TRex/data"
+dest_root = "/Users/mmchenry/Documents/Projects/waketracking/data"
+
+# Directories to exclude
+excluded_dirs = ['.DS_Store','Test',"TestSchoolBhav","miniScaleRN","RN_Prop_prelim"]
+
+# Get list of project directories, excluding unwanted ones
+project_dirs = [dir for dir in os.listdir(src_root) if dir not in excluded_dirs]
+total_projects = len(project_dirs)
+
+# Iterate through project directories in the source root
+for idx, project_dir in enumerate(project_dirs):
+    print(f"Processing {idx+1}/{total_projects}: {project_dir}")
+    
+    src_project_path = os.path.join(src_root, project_dir)
+    dest_project_path = os.path.join(dest_root, project_dir)
+    
+    # Make sure the destination project directory exists
+    if not os.path.exists(dest_project_path):
+        os.makedirs(dest_project_path)
+    
+    # Copy 'experiment_log.csv'
+    src_csv = os.path.join(src_project_path, 'experiment_log.csv')
+    dest_csv = os.path.join(dest_project_path, 'experiment_log.csv')
+    if os.path.exists(src_csv):
+        shutil.copy(src_csv, dest_csv)
+
+    # # Copy 'recording_log.csv'
+    # src_csv = os.path.join(src_project_path, 'recording_log.csv')
+    # dest_csv = os.path.join(dest_project_path, 'recording_log.csv')
+    # if os.path.exists(src_csv):
+    #     shutil.copy(src_csv, dest_csv)
+    
+    # Copy files in 'matlab/centroid' ending with *_rawfish.mat
+    src_matlab_path = os.path.join(src_project_path, 'matlab', 'centroid')
+    dest_matlab_path = os.path.join(dest_project_path, 'matlab', 'centroid')
+    
+    # MATLAB FILES
+    if os.path.exists(src_matlab_path):
+        # Create destination matlab/centroid folder if it doesn't exist
+        if not os.path.exists(dest_matlab_path):
+            os.makedirs(dest_matlab_path)
+        
+        # Iterate through all files and copy the ones that match our criteria
+        for filename in os.listdir(src_matlab_path):
+            if filename.endswith('_rawfish.mat'):
+                src_file = os.path.join(src_matlab_path, filename)
+                dest_file = os.path.join(dest_matlab_path, filename)
+                shutil.copy(src_file, dest_file)
+
+        for filename in os.listdir(src_matlab_path):
+            if filename.endswith('_schooldata.mat'):
+                src_file = os.path.join(src_matlab_path, filename)
+                dest_file = os.path.join(dest_matlab_path, filename)
+                shutil.copy(src_file, dest_file)
+
+        for filename in os.listdir(src_matlab_path):
+            if filename.endswith('_focalfish.mat'):
+                src_file = os.path.join(src_matlab_path, filename)
+                dest_file = os.path.join(dest_matlab_path, filename)
+                shutil.copy(src_file, dest_file)
+
+    # Copy files from 'masks' 
+    src_masks_path = os.path.join(src_project_path, 'masks')
+    dest_masks_path = os.path.join(dest_project_path, 'masks')
+
+    # MASK FILES
+    if os.path.exists(src_masks_path):
+        # Create destination matlab/centroid folder if it doesn't exist
+        if not os.path.exists(dest_masks_path):
+            os.makedirs(dest_masks_path)
+
+        # Iterate through all files and copy the ones that match our criteria
+        for filename in os.listdir(src_masks_path):
+            src_file = os.path.join(src_masks_path, filename)
+            dest_file = os.path.join(dest_masks_path, filename)
+            shutil.copy(src_file, dest_file)
